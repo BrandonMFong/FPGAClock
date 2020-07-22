7c62cba4e19b8672d5ccded8ef668f6ac91e3849
BrandonMFong <fong.m.brandon97@gmail.com>
2020-07-21 17:53:12 -0700

    Setting up basic controls for a clock

--- FPGAClock.srcs/sources_1/new/ControlCenter.v
+++ FPGAClock.srcs/sources_1/new/ControlCenter.v
@@ -32,8 +32,22 @@ module ControlCenter
     
     // Data signals
     output          reset, // Resets clock back to 
+                    // TODO find better names
+                    increase, // Increases time
+                    decrease, // Decreases time
     
     // State signals
     output          MODE_Setup
 );
+
+    // When any of the inputs changes its state, it will enter this always block
+//    always @(*)
+//    begin
+//        if(btnC) 
+//    end
+
+    assign MODE_Setup = sw[0] ? 1 : 0;
+    assign reset = (sw[15] && btnC) ? 1 :0;
+    assign increase = (MODE_Setup && btnU) ? 1 : 0;
+    assign decrease = (MODE_Setup && btnD) ? 1 : 0;
 endmodule
